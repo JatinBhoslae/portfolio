6df378dd297624bb07373591e7981150faaed17f
Jatin <jatinbhosale428@gmail.com>
2025-08-25 00:06:17 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-app/',
+  base: process.env.VITE_BASE_PATH || "/portfolio",
   build: {
     outDir: 'dist',
   },
